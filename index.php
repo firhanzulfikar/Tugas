47aed4b0e65e53804676ecd28a96b6fe71fdd3c9
firhanzulfikar <99271110+firhanzulfikar@users.noreply.github.com>
2022-02-08 22:13:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include("koneksi.php"); ?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LKP TIKOM</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+    <div class="judul">
+        <b><h2>LKP TIKOM</h2>
+        <h2>LEMBAGA KURSUS DAN PELATIHAN</h2></b>
+    </div>
+
+    <br>
+    <p><center>Selamat Datang Di Website Lembaga Kursus</center></p>
+    <nav>
+			<b><a href="list_daftar.php">Daftar Kursus</a><br>
+			<a href="list_bidang.php">Bidang Kursus</a><br>
+			<a href="list_instruktur.php">Instruktur Kursus</a><br></b>
+	</nav><br><br><br>
+	<div class="judul"><center><footer><strong>Created by | FIRHAN ZULFIKAR 2019</strong></footer></center></div>
+
+</body>
+</html>
+    
